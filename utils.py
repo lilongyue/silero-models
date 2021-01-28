4c837bf68202a18d9da2eca06f8eb68143e5523f
lilongyue <longyuelimail@gmail.com>
2021-01-28 15:39:51 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from typing import List
 from itertools import groupby
 
 
-torchaudio.set_audio_backend("soundfile")  # switch backend
+torchaudio.set_audio_backend("sox")  # switch backend
 
 
 def read_batch(audio_paths: List[str]):
